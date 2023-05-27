7f7d165161f7b839afab4e900123ff2b6b9f7c3c
Nick Kipshidze <96648005+NickKipshidze@users.noreply.github.com>
2023-05-27 09:58:52 +0400

    Makes new sentences

---
+++ main.py
@@ -0,0 +1,56 @@
+import random, time
+
+def count_words(text: str) -> dict:
+    frequency: dict = {}
+    words: list = text.lower().split(" ")
+
+    for index, word in enumerate(words):
+        if words.count(word) > 1:
+            if word in frequency:
+                frequency[word].append(index)
+            else:
+                frequency[word] = [index]
+    
+    return frequency
+
+def generate_preview(content: str) -> None:
+    preview_dir = "./preview"
+    template = open(f"{preview_dir}/layout.html", "r").read()
+    
+    index = template.replace("{{ content }}", content)
+
+    open(f"{preview_dir}/index.html", "w").write(index)
+
+def main() -> None:
+    text: str = open("./text.txt", "r").read()
+
+    words: list = text.lower().split(" ")
+    frequency: dict = count_words(text)
+
+    print("TEXT LENGTH    :", len(text))
+    print("DATABASE WORDS :", len(words))
+    print("NODES          :", len(frequency))
+    input("")
+
+    print("GENERATED:", end=" ")
+
+    generation = ""
+
+    try:
+        word: int = random.randint(len(words)//4, len(words))
+        while word < len(words):
+            print(words[word], end = " ", flush = True)
+            generation += words[word] + " "
+
+            if words[word] in frequency:
+                word = random.choice(frequency[words[word]])
+                time.sleep(0.1)
+            
+            word += 1
+    except KeyboardInterrupt:
+        print("\n\nStopping...")
+    
+    generate_preview(generation)
+
+if __name__ == "__main__":
+    main()

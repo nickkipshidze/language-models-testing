7e567a79899468fbf85a65e19c8063bfc63d637e
NickKipshidze <kipshidze.nick@gmail.com>
2023-05-31 00:02:06 +0400

    Cleaner code

--- multigrams/main.py
+++ multigrams/main.py
@@ -1,18 +1,19 @@
-import random
+import random, time
 
 class Multigrams:
     def __init__(self, dataset_file: str) -> None:
         print(f"[Multigrams] Reading '{dataset_file}';")
 
         self.dataset: list = \
-            open(dataset_file, "r").read().lower().split(" ")
+            open(dataset_file, "r").read().lower().replace("\n", "").split(" ")
 
         print(f"[Multigrams] Dataset initialized;")
     
     def generate_multigram(self, group_size: int) -> dict:
         print(f"[Multigrams] Generating multigram, {group_size=};")
 
-        multigram: dict = {}
+        self.multigram: dict = {}
+        self.group_size: int = group_size
 
         # Create a multigram
         for index in range(len(self.dataset)):
@@ -21,19 +22,15 @@ class Multigrams:
             # If we hit the end of the dataset
             if len(group) < group_size: break
             
-            if " ".join(group[:-1]) in multigram:
-                multigram[
+            if " ".join(group[:-1]) in self.multigram:
+                self.multigram[
                     " ".join(group[:-1])
-                ].append(index)
+                ].append(group[-1])
 
             else:
-                multigram[
+                self.multigram[
                     " ".join(group[:-1])
-                ] = [index]
-        
-        # Clean dict from empty values
-        self.multigram: dict = {key: value for key, value in multigram.items() if value}
-        self.group_size: int = group_size
+                ] = [group[-1]]
         
         print("[Multigrams] Multigram generated;")
 
@@ -42,22 +39,24 @@ class Multigrams:
     def generate_next(self, params: list) -> str:
         # Convert list to string
 
+        # Potential key in multigram
         params: str = " ".join(
             params[-(self.group_size - 1):]
         )
 
         if params in self.multigram:
             # If the parameters were found in multigram
+            
+            # Example of self.multigram:
+            #     self.multigram = {"parameter1 parameter2": ["parameter3", "parameter2.5"]}
 
-            return self.dataset[
-                random.choice(
-                    self.multigram[params]
-                ) + (self.group_size - 1)
-            ]
-        
+            return random.choice(
+                self.multigram[params]
+            )        
         else:
             # If the parameters were not found in multigram
 
+            # Find any occurrence of the last parameter and return word after it
             return self.dataset[
                 self.dataset.index(
                     params.split(" ")[-1]
@@ -70,10 +69,13 @@ def main() -> None:
 
     generation: list = input("\nPrompt: ").lower().split(" ")
 
+    print(" ".join(generation), end = " ")
     while generation[-1][-1] != ".":
         generation.append(multigram.generate_next(generation))
+        print(generation[-1], end = " ", flush = True)
+        time.sleep(0.1)
 
-    print(" ".join(generation))
+    print("\n")
 
 if __name__ == "__main__":
     main()
